7e608ba6b6fc360975d9f174e79c89781b6098dc
cre <cre@chriseliot.com>
2021-08-01 18:03:50 -0400

    : Capture all Inferences
    Description: Eliminate some debug code

--- com.chriseliot.geo/src/main/java/com/chriseliot/geo/gui/GeoControls.java
+++ com.chriseliot.geo/src/main/java/com/chriseliot/geo/gui/GeoControls.java
@@ -1,8 +1,6 @@
 
 package com.chriseliot.geo.gui;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Point2D;
@@ -338,17 +336,8 @@ public class GeoControls extends JPanel implements ActionListener
         else if (tag.equals ("GeoVertex"))
         {
             final Color color = new Color (xu.getInteger (element, "color", 0));
-            final String name1 = xu.get (element, "line1", null);
-            final String name2 = xu.get (element, "line2", null);
-            assertNotNull (name1);
-            assertNotNull (name2);
-            logger.info ("Vertex %s", xu.get (element, "name", null));
-            logger.info ("Line1 %s", name1);
-            logger.info ("Line2 %s", name2);
-            final GeoLine line1 = (GeoLine)plane.get (name1);
-            final GeoLine line2 = (GeoLine)plane.get (name2);
-            assertNotNull (line1);
-            assertNotNull (line2);
+            final GeoLine line1 = (GeoLine)plane.get (xu.get (element, "line1", null));
+            final GeoLine line2 = (GeoLine)plane.get (xu.get (element, "line2", null));
             final GeoVertex item = new GeoVertex (plane, color, line1, line2, new Point2D.Double (0, 0));
             plane.addItem (item);
             item.marshall (element);
